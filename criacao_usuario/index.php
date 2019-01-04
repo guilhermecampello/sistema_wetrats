bf207cab062484ba84aeb20436c95adacb874235
Guilherme Campello <gui.campello98@gmail.com>
2019-01-04 18:49:46 -0200

    form de criacao de usuario funcional

--- criacao_usuario/index.php
+++ criacao_usuario/index.php
@@ -2,17 +2,23 @@
   <link rel="stylesheet" type="text/css" href="../common/stylesheets/styles.css">
 </head>
 
-<form action="usuario.php" style="border:1px solid #ccc">
+<form method = "post" action="index.php" style="border:1px solid #ccc">
   <div class="container">
     <h1>Novo usuário</h1>
     <p>Preencha os campos abaixo para criar seu perfil.</p>
     <hr>
 
-    <label for="Nome"><b>Nome Completo</b></label>
-    <input type="text" placeholder="Digite seu nome e sobrenome..." name="Nome" required>
+    <label for="nome"><b>Nome Completo</b></label>
+    <input type="text" placeholder="Digite seu nome e sobrenome..." name="nome" required>
+
+    <label>
+      <input type="radio" name="sexo" value="M"> Masculino <br>
+      <input type="radio" name="sexo" value="F"> Feminino <br>
+    </label>
+
     
     <label for="Data de Nascimento"><b>Data de Nascimento</b></label>
-    <input type="date" name="data de nascimento" max="1979-12-31" min=" 2004-12-31" required><br>
+    <input type="date" name="data_de_nascimento" min="1979-12-31" max=" 2004-12-31" required><br>
     
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Digite seu Email..." name="email" required>
@@ -30,13 +36,13 @@
     <input type="text" placeholder="Digite seu RG..." name="RG" required>
 
     <label for="Número USP"><b>Número USP</b></label>
-    <input type="text" placeholder="Digite seu Número USP..." name="Número USP" >
+    <input type="text" placeholder="Digite seu Número USP..." name="NUSP" >
 
     <label for="Endereço"><b>Endereço</b></label>
-    <input type="text" placeholder="Digite seu Endereço..." name="Endereço" >
+    <input type="text" placeholder="Digite seu Endereço..." name="endereco" >
 
     <label>
-      <input type="radio" name="nivel" value="1"> Alteta <br>
+      <input type="radio" name="nivel" value="1"> Atleta <br>
       <input type="radio" name="nivel" value="2"> Técnico <br>
       <input type="radio" name="nivel" value="3"> DM <br>
     </label>
@@ -44,7 +50,27 @@
 
     <div class="clearfix">
       <button type="button" class="cancelbtn">Cancelar</button>
-      <button type="button" class="signupbtn">Criar</button>
+      <input type="submit" class="signupbtn">Criar</>
     </div>
   </div>
 </form>
+
+<?php
+
+if ($_POST){
+  $nome = $_POST['nome'];
+  $sexo = $_POST['sexo'];
+  $data_de_nascimento = $_POST['data_de_nascimento'];
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
+  $celular = $_POST['celular'];
+  $RG = $_POST['RG'];
+  $NUSP = $_POST['NUSP'];
+  $endereco = $_POST['endereco'];
+  $nivel = $_POST['nivel'];
+  $con = mysqli_connect('localhost', 'root', '', 'sistema_wetrats');
+  $sql = "INSERT INTO usuarios (nome,sexo,data_de_nascimento,email,senha,celular,RG,NUSP,endereco,Nivel) VALUES ('".$nome."','".$sexo."','".$data_de_nascimento."','".$email."','".$senha."','".$celular."','".$RG."','".$NUSP."','".$endereco."','".$nivel."')";
+  $query = mysqli_query($con,$sql);
+  echo "$sql";
+  }
+?>
